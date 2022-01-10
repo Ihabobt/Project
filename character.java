4cc3ca9b3eb4394ca1299f0c3030c1987b01c8d8
Ihabobt <62311874+Ihabobt@users.noreply.github.com>
2022-01-10 14:28:42 -0800

    Update character.java

--- character.java
+++ character.java
@@ -1,3 +1,21 @@
+import java.util.Random;
 public class character
 {
-  
+	public class character
+
+    private short shrCurrentHealth = 0; 
+    private short shrMaxHealth= 0; 
+    private byte bytArmor = 0; 
+    private byte bytAccuracy = 0; 
+    private byte  bytRandomDamage;  
+    private byte bytDamageBonus = 2; 
+	
+	Random dice = new Random();
+	bytRandDamage = 1 + dice.nextInt(8);
+	
+	character (byte armor, byte accuracy, byte RandomDamage, byte DamgeBonus)
+	byte armor = this.shrCurrentHealth
+	byte accuracy = this.bytAccuracy
+	byte RandomDamge = this.bytRandomDamge
+	byte DamageBonus = this.bytDamageBonus
+}
