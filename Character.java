f7683493bea006e3c87db78f9a9798ac29b87b08
Ihabobt <62311874+Ihabobt@users.noreply.github.com>
2022-01-10 18:12:32 -0800

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,26 +1,27 @@
 import java.util.Random;
 public class Character
 {
-  public class Character private short shrCurrentHealth = 0;
+  private short shrCurrentHealth = 0;
   private short shrMaxHealth = 0;
   private byte bytArmor = 0;
   private byte bytAccuracy = 0;
   private byte bytRandomDamage;
   private byte bytDamageBonus = 2;
-
+  private byte bytDamageDie = 0;
   Random dice = new Random ();
-    bytRandDamage = 1 + dice.nextInt (8);
+bytRandomDamage = ((1 + dice.nextInt (bytDamageDie)) + bytDamageBonus);
 
-    Character (byte armor, byte accuracy, byte RandomDamage, byte DamgeBonus)
-    byte armor = this.shrCurrentHealth
-    byte accuracy = this.bytAccuracy
-    byte RandomDamge = this.bytRandomDamge
-    byte DamageBonus = this.bytDamageBonus
+    Character (byte armor, byte accuracy, byte DamageDie, byte DamageBonus);
+    byte armor = this.bytArmor;
+    byte accuracy = this.bytAccuracy;
+    byte RandomDamage = this.bytRandomDamage;
+    byte DamageBonus = this.bytDamageBonus;
     
-    Character ()byte armor = 0
-    byte accuracy = 0
-    byte RandomDamge = 0
-    byte DamageBonus = 0 
+    Character (){
+    byte armor = 0;
+    byte accuracy = 0;
+    byte RandomDamge = 0;
+    byte DamageBonus = 0 ;
 }
    public void changeHealth (short change) {
     shrCurrentHealth = shrCurrentHealth + change;
@@ -38,7 +39,19 @@ public class Character
  
  public byte attack(byte targetArmor) {
     
-    if (targetArmor> (bytAccuracy + (dice.nextInt(20) + 1) ) 
-    targetArmor =
-     
+    if (targetArmor> (bytAccuracy + (dice.nextInt(20) + 1) )) { 
+    
+        return 0;
+    } else   {
+    
+    } return bytRandomDamage;
+    }
+        
+    
+    public byte getArmor()  //getters methods
+    {
+        return armor;
+    }
+ 
  }
+ 
