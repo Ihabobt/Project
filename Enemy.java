ce10949c742065ec6965c9292182fa5d9a8a6b48
nikjoniec <97474934+nikjoniec@users.noreply.github.com>
2022-01-11 12:53:05 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,15 +1,9 @@
 public class Enemy extends Character
 {
-  //declaring instance variables
- 
-  Private byte bytArmor;
-  Private byte bytAccuracy;
-  Private byte 
-    //Constructor
-    Enemy(byte round, String name)
+  
+  //Constructor
+  Enemy(byte round, String name)
     {
-      this.strName = name;
-      
       //calling super constructor and scaling enemy 
       super(10+(3*round), );
       
