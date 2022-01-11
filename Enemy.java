3c5d5eee1e80cf2309aee36e3c59f6ecdfffb29a
nikjoniec <97474934+nikjoniec@users.noreply.github.com>
2022-01-11 13:15:40 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,11 +1,10 @@
 public class Enemy extends Character
 {
-  
   //Constructor
   Enemy(byte round, String name)
     {
       //calling super constructor and scaling enemy 
-      super(10+(3*round), 10+(3*round), 10+(3*round), 10+(3*round));
+      super(name, 10+(3*round), 10+(3*round), 10+(3*round), 10+(3*round));
       
       
    }
